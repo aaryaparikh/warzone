d2c07d805df124bb4b275b5b12c45cfa3573c813
Dev Pandya <71974172+DP8801@users.noreply.github.com>
2023-10-04 11:29:32 -0400

    Added Continent Test case

---
+++ src/test/java/Models/ContinentTest.java
@@ -0,0 +1,55 @@
+package Models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ContinentTest {
+
+	private Continent t_continent;
+
+	/**
+	 * sets up <i>t_continent</i> variable of type Continent and initialize it with
+	 * predefined values
+	 */
+	@BeforeEach
+	public void setup() {
+		this.t_continent = new Continent(1, 5);
+	}
+
+	/**
+	 * tears down the <i>t_continent</i> variable by setting it to null
+	 */
+	@AfterEach
+	public void teardown() {
+		this.t_continent = null;
+	}
+
+	/**
+	 * tests the getter <i>getContinentId</i> and test that it returns valid value
+	 */
+	@Test
+	public void should_ReturnContinentID() {
+		// given
+		int l_expectedContinentID = 1;
+
+		// then
+		assertEquals(l_expectedContinentID, t_continent.getContinentId());
+	}
+
+	/**
+	 * tests the getter <i>getContinentValue</i> and test that it returns valid
+	 * value
+	 */
+	@Test
+	public void should_ReturnContinentValue() {
+		// given
+		int l_expectedContinentValue = 5;
+
+		// then
+		assertEquals(l_expectedContinentValue, t_continent.getContinentValue());
+	}
+
+}
