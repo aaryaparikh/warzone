2396dcb5d2f392537477adb0c6b270a73eb8bb81
Virag Patel <19it114@charusat.edu.in>
2023-11-08 13:15:58 -0500

    Added the Test suite for the Order validation Test cases

--- src/test/java/Models/ContinentTest.java
+++ src/test/java/Models/ContinentTest.java
@@ -1,6 +1,7 @@
 package Models;
 
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
---
+++ src/test/java/Models/Orders/OrderTestSuite.java
@@ -0,0 +1,11 @@
+package Models.Orders;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses ({AdvanceOrderTest.class, AirliftOrderTest.class, BlockadeOrder.class, DeployOrderTest.class, DiplomacyOrderTest.class, AirliftOrderTest.class })
+public class OrderTestSuite {
+
+}
+
