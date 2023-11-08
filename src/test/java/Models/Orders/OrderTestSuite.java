981171e4b58222e958349b6eeab52c23babcb202
Virag Patel <19it114@charusat.edu.in>
2023-11-08 14:09:49 -0500

    updated the OrderTestSuite

--- src/test/java/Models/Orders/OrderTestSuite.java
+++ src/test/java/Models/Orders/OrderTestSuite.java
@@ -1,5 +1,9 @@
+/**
+ * JUnit 5 TestSuite class for all the classes testing the order phase.
+ *
+ * @author Virag
+ */
 package Models.Orders;
-
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
