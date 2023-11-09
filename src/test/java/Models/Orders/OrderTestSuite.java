b1c354a8a2756fc7e6db313700eb1a85585ec01a
Virag Patel <19it114@charusat.edu.in>
2023-11-08 22:37:51 -0500

    Updated the java doc of necessary files

--- src/test/java/Models/Orders/OrderTestSuite.java
+++ src/test/java/Models/Orders/OrderTestSuite.java
@@ -1,12 +1,26 @@
-/**
- * JUnit 5 TestSuite class for all the classes testing the order phase.
- *
- * @author Virag
- */
+
 package Models.Orders;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+
+/**
+ * This JUnit 5 TestSuite class encompasses tests for the various order-related functionalities in the 'Models.Orders' package.
+ * It includes test classes for AdvanceOrder, AirliftOrder, BlockadeOrder, DeployOrder, and DiplomacyOrder.
+ *
+ * Usage:
+ * To execute all the order-related tests, run this class as a JUnit test.
+ *
+ * Example:
+ * <pre>
+ * {@code
+ * OrderTestSuite suite = new OrderTestSuite();
+ * JUnitCore.runClasses(suite.getClass());
+ * }
+ * </pre>
+ *
+ * @author Virag
+ */
 @Suite
 @SelectClasses ({AdvanceOrderTest.class, AirliftOrderTest.class, BlockadeOrder.class, DeployOrderTest.class, DiplomacyOrderTest.class, AirliftOrderTest.class })
 public class OrderTestSuite {
--- src/test/java/Utils/UtilsTestSuite.java
+++ src/test/java/Utils/UtilsTestSuite.java
@@ -2,7 +2,16 @@ package Utils;
 
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
-
+/**
+ * This class represents a JUnit 5 test suite for the Utils package. 
+ * It combines multiple test classes related to the Utils package into a single suite.
+ * The included test classes are MapCommandHandlerTest and PlayerCommandHandlerTest.
+ * 
+ * Usage:
+ * To execute all the tests within this suite, run this class as a JUnit test.
+ * 
+ * @author Virag
+ */
 @Suite
 @SelectClasses({ MapCommandHandlerTest.class, PlayerCommandHandlerTest.class })
 public class UtilsTestSuite {
