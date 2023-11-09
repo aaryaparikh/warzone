9ad43c1f93ceb01a7e5953e9e418136c7dcc8c22
Virag Patel <19it114@charusat.edu.in>
2023-11-08 22:44:19 -0500

    updated the java doc comments of modelTestSuite

--- src/test/java/Models/ModelTestSuite.java
+++ src/test/java/Models/ModelTestSuite.java
@@ -4,9 +4,21 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 /**
- * JUnit 5 Test Suite Class for running all the Test Cases of Models.
+ * This JUnit 5 Test Suite Class is responsible for running all the test cases related to the Models package.
+ * It includes test classes for Continent, Country, GameMap, and Player.
  *
- * @author Virag
+ * Usage:
+ * To execute all the Models package tests, run this class as a JUnit test.
+ *
+ * Example:
+ * <pre>
+ * {@code
+ * ModelTestSuite suite = new ModelTestSuite();
+ * JUnitCore.runClasses(suite.getClass());
+ * }
+ * </pre>
+ *
+ * @author <b>Virag</b>
  */
 @Suite
 @SelectClasses({ ContinentTest.class, CountryTest.class, GameMapTest.class, PlayerTest.class })
