540baae638bf1689281878509851aa71acec7719
Dev Pandya <71974172+DP8801@users.noreply.github.com>
2023-11-29 13:52:37 -0500

    Updating UtilsTestSuite

--- src/test/java/Utils/UtilsTestSuite.java
+++ src/test/java/Utils/UtilsTestSuite.java
@@ -7,14 +7,14 @@ import org.junit.platform.suite.api.Suite;
  * This class represents a JUnit 5 test suite for the Utils package. It combines
  * multiple test classes related to the Utils package into a single suite. The
  * included test classes are MapCommandHandlerTest and PlayerCommandHandlerTest.
- * 
+ *
  * Usage: To execute all the tests within this suite, run this class as a JUnit
  * test.
- * 
+ *
  * @author Virag
  */
 @Suite
-@SelectClasses({ MapCommandHandlerTest.class, PlayerCommandHandlerTest.class })
+@SelectClasses({ MapCommandHandlerTest.class, PlayerCommandHandlerTest.class, MapEditorTest.class })
 public class UtilsTestSuite {
 
 }
