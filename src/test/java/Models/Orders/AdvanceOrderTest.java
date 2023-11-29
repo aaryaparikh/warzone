d13a1e7f3185b20004ec35bc3ecfe80c145e3b7c
Virag Patel <19it114@charusat.edu.in>
2023-11-29 14:08:00 -0500

    added 2 new test cases for the AdvanceOrderTest

--- src/test/java/Models/Orders/AdvanceOrderTest.java
+++ src/test/java/Models/Orders/AdvanceOrderTest.java
@@ -1,5 +1,6 @@
 package Models.Orders;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -216,4 +217,65 @@ public class AdvanceOrderTest {
 				() -> assertEquals(d_p1.getD_reinforcementPool(), l_source.getArmies() + l_target.getArmies()));
 
 	}
+	
+	
+
+	/**
+     * Test the scenario where the target country is not a neighbor of the source country.
+     * @author Virag
+     */
+	
+	@Test
+    public void shouldReturnString_For_NonNeighborTargetCountry() {
+        Country source = getSource();
+        Country nonNeighborTarget = d_gameEngine.getGameMap().getCountries().stream()
+                .filter(c -> !source.getNeighborCountries().contains(c.getCountryId()))
+                .findFirst()
+                .orElseThrow(); // Assumes there is at least one non-neighbor country
+
+        int armies = 5;
+
+        d_depOrder = new DeployOrder(d_p1, source, armies);
+        d_depOrder.execute(d_gameEngine);
+
+        d_advOrder = new AdvanceOrder(d_p1, source, nonNeighborTarget, armies);
+        String result = d_advOrder.execute(d_gameEngine);
+
+        assertEquals(String.format(
+                "Armies can't be moved from country \"%d\" to country \"%d\" because they are not neighbors",
+                source.getCountryId(), nonNeighborTarget.getCountryId()), result);
+    }
+	
+	/**
+     * Test the scenario where the target country is neutral.
+     * @author Virag
+     */
+	@Test
+	public void shouldConquerNeutralTargetCountry() {
+	    Country source = getSource();
+
+	    // Find a neutral country or create one if none is present
+	    Country neutralTarget = d_gameEngine.getGameMap().getCountries().stream()
+	            .filter(c -> c.getOwner() == null)
+	            .findFirst()
+	            .orElseGet(() -> {
+	                Country newNeutralCountry = new Country(1,1);
+	                d_gameEngine.getGameMap().addCountry(1,1);
+	                return newNeutralCountry;
+	            });
+
+	    int armies = source.getArmies();
+
+	    d_depOrder = new DeployOrder(d_p1, source, armies);
+	    d_depOrder.execute(d_gameEngine);
+
+	    d_advOrder = new AdvanceOrder(d_p1, source, neutralTarget, armies);
+	    String result = d_advOrder.execute(d_gameEngine);
+
+	    assertFalse(result.contains(String.format("\"%d\" armies are moved from country \"%d\" to country \"%d\"",
+	            armies, source.getCountryId(), neutralTarget.getCountryId())));
+
+	}
+
+
 }
